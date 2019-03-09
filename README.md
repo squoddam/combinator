524afd8f87de2566e080b903f62321d1ccf3cdb0
Elazar Kopyrin <s.quoddam@gmail.com>
2019-03-09 20:31:03 +0200

    Add README

---
+++ README.md
@@ -0,0 +1,4 @@
+\*\* Combinator
+
+Allows to create all possible combinations, based on provided properties
+and their possible values
--- utils.js
+++
@@ -1,8 +0,0 @@
-export const log = (...args) => {
-  if (args.length === 2) {
-    const [message, value] = args;
-    return console.log(message, value), value;
-  }
-
-  return console.log(args[0]), args[0];
-};
